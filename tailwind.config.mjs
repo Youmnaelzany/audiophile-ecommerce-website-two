ebfc174b3a82fb9d05cf98ae4eebeea9b07a3162
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-02-13 01:21:41 +0200

    another version of the project

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        "ZX7-SPEAKER": "url('/images/home/desktop/image-speaker-zx7.jpg')",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
